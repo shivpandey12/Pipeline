3091e411b2198f2bb32425e4756a3bb237e6264b
shivpandey12 <121170352+shivpandey12@users.noreply.github.com>
2024-06-11 18:12:32 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.47.0"
+    }
+  }
+}
+provider "google" {
+  region = "asia-east1"
+  zone   = "asia-east1-a"
+}
